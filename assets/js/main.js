ca570d9dac4c2ebff01a726a806da15402d7745a
Bazilik <vladkitoroga1095@gmail.com>
2023-12-02 00:16:58 +0200

    UPD

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,5 +5,22 @@ $(function () {
      $(window).on('load', function () {
         $(".preloader").fadeOut();
     });
+
+    $(".toogle-button-1").on('click',function(){
+        $(".toogle-content-1").slideToggle('slow');
+        $(this).toggleClass("active");
+    })
+    $(".toogle-button-2").on('click',function(){
+        $(".toogle-content-2").slideToggle('slow');
+        $(this).toggleClass("active");
+    })
+    $(".toogle-button-3").on('click',function(){
+        $(".toogle-content-3").slideToggle('slow');
+        $(this).toggleClass("active");
+    })
+    $(".toogle-button-4").on('click',function(){
+        $(".toogle-content-4").slideToggle('slow');
+        $(this).toggleClass("active");
+    })
 });
 
